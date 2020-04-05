862daa27fd232d29d78a6c668d715c864f2f2b6e
Junlee321 <curlyjun@gmail.com>
2020-04-05 15:54:28 +0800

    Setup Terrain Texter and space ship

---
+++ Assets/Splash.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Splash : MonoBehaviour
+{
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadNextLevel", 2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
+
