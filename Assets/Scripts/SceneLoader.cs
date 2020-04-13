270a09946ce11c59e335ef0d3ea45961cc706bb1
Junlee321 <curlyjun@gmail.com>
2020-04-13 18:00:00 +0800

    Rreorganize folder

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,13 +1,10 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Splash : MonoBehaviour
+public class SceneLoader : MonoBehaviour
 {
-
-    void Awake()
-    {
-        DontDestroyOnLoad(gameObject);
-    }
     // Start is called before the first frame update
     void Start()
     {
@@ -17,12 +14,10 @@ public class Splash : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        
     }
-
     void LoadNextLevel()
     {
         SceneManager.LoadScene(1);
     }
 }
-
---
+++ Assets/Scripts/Splash.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+
+public class Splash : MonoBehaviour
+{
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
+
